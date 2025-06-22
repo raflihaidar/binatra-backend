6ad0c58d6c052e2126529ab494fad8812bb67d81
Rafli Haidar Nashif <rafli.haidar30@gmail.com>
2025-06-22 07:58:27 +0700

    feat : add credential

--- src/config/mqtt.js
+++ src/config/mqtt.js
@@ -3,11 +3,17 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export const mqttConfig = {
   host: process.env.APP_HOST,
-  port : process.env.APP_PROTOCOL_PORT,
-  protocol : process.env.APP_PROTOCOL,
+  port: Number(process.env.APP_PROTOCOL_PORT),
+  protocol: process.env.APP_PROTOCOL,
+  ...(isProduction && {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD,
+  }),
   topics: {
     waterLevel: 'esp32/sensor/waterlevel',
   }
-};
+}
