aa19fea6c5f5619033b67163d7b46ce474373664
Rafli Haidar Nashif <rafli.haidar30@gmail.com>
2025-07-30 20:38:53 +0700

    fix : fix production handler in pino library

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,15 +1,21 @@
-import pino from "pino"
+import pino from "pino";
 
-const logger = pino({
-    transport: {
-      target: process.env.NODE_ENV === 'production' ? '' : 'pino-pretty',  
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-      },
-    },
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  })
-  
-  export default logger
+const isProd = process.env.NODE_ENV === "production";
+
+const logger = pino(
+  isProd
+    ? {}
+    : {
+        transport: {
+          target: process.env.NODE_ENV === "production" ? "" : "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+          },
+        },
+        level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      }
+);
+
+export default logger;
